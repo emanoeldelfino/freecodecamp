a4a65287057180b04f56b39935ae2ec5146c3d94
emanoeldelfino <emanoeldelfno@gmail.com>
2022-03-06 00:59:46 -0300

    Update

--- projects/3-product-landing-page/app.js
+++ projects/3-product-landing-page/app.js
@@ -3,7 +3,7 @@ let i = 1;
 const slide = document.querySelector("section#bottles img");
 
 function changeSlide() {
-  slide.src = `./assets/slider${i}.jpg`;
+  slide.src = `./assets/bottle${i}.jpg`;
   i = i < 3 ? i + 1 : 1;
   setTimeout(changeSlide, 3000);
 }
